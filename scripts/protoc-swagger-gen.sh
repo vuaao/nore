2333c0ec3f135fac1199a9d25f2c9eadc5fd0333
chen <aaa445366516@gmail.com>
2023-08-05 14:54:28 +0800

    Update protoc-swagger-gen.sh

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
-
+#
 mkdir -p ./tmp-swagger-gen
 proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
