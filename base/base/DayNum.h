4abaf07f8b729ded44f7497bcda96da21a10c581
chen <aaa445366516@gmail.com>
2023-09-21 17:03:33 +0800

    Update DayNum.h

--- base/base/DayNum.h
+++ base/base/DayNum.h
@@ -6,7 +6,6 @@
   * See DateLUTImpl for usage examples.
   */
 STRONG_TYPEDEF(UInt16, DayNum)
-
 /** Represent number of days since 1970-01-01 but in extended range,
  * for dates before 1970-01-01 and after 2105
  */
